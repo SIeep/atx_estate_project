60dafd788285e1bb76cdf73303d837d2c7787820
sIeep <bevans@capitolscientific.com>
2020-04-20 09:02:26 -0500

    add postgres db and Listing model

--- btre/settings.py
+++ btre/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-from secret_settings import SUPER_SECRET_KEY
+from secret_settings import SUPER_SECRET_KEY, POSTGRES_PASS, POSTGRES_USER, POSTGRES_DB
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': POSTGRES_DB,
+        'USER': POSTGRES_USER,
+        'PASSWORD': POSTGRES_PASS,
+        'HOST': 'localhost'
     }
 }
 
