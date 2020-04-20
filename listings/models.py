53d5eb43cb1a461b01e000beaea304ed4f13ecfc
sIeep <bevans@capitolscientific.com>
2020-04-20 09:26:00 -0500

    add Realtor model and register to admin

--- listings/models.py
+++ listings/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from datetime import datetime
 from realtors.models import Realtor
 
-# Create your models here.
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
